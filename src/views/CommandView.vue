edc96ecfa867acf13da93a540ccf4d561fed7fb3
Evan Heaton <epheat07+github@gmail.com>
2020-12-23 12:18:03 -0800

    add router

---
+++ src/views/CommandView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="command-view">
+    Command View
+    <control-board />
+  </div>
+</template>
+<script>
+import ControlBoard from '../components/ControlBoard.vue'
+export default {
+  components: {
+    ControlBoard
+  }
+}
+</script>
