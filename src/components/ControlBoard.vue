150e21be196bd0838b889264a5498d9ca3d0bdf4
Evan Heaton <epheat07+github@gmail.com>
2020-12-22 19:26:09 -0800

    add routes and axios to call them

---
+++ src/components/ControlBoard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="control-board">
+    Control Board contents here - {{foo}}.
+  </div>
+</template>
+<script>
+export default {
+    data: function() {
+        return {
+            foo: "bar"
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
--- srv/index.js
+++ srv/index.js
@@ -1,16 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+import surveyHandler from './routes/survey.js';
+import gameHandler from './routes/game.js';
 // import socketIO from "socket.io";
 
 export default (app, http) => {
-  // app.use(express.json());
-  //
-  // app.get('/foo', (req, res) => {
-  //   res.json({msg: 'foo'});
-  // });
-  //
-  // app.post('/bar', (req, res) => {
-  //   res.json(req.body);
-  // });
+  app.use(express.json());
+  app.use(cors());
+
+  // set up routes here
+  app.use('/survey', surveyHandler);
+  app.use('/game', gameHandler);
+
   // 
   // optional support for socket.io
   // 
